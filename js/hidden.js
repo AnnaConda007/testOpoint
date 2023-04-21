9b7b4d40f6f01ac54bc64cd64a92c46fdb6bf876
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-21 20:12:05 +0400

    hidden

--- js/hidden.js
+++ js/hidden.js
@@ -1,17 +1,20 @@
-
 const visible = () => {
-    setTimeout(() => {
-        const hiddenElements = document.querySelectorAll(".hidden");
-        hiddenElements.forEach((el) => {
-            el.classList.add("visible");
-        });
-        const hiddenElement = document.querySelector(".hidden");
-        hiddenElement.style.opacity = 1
-    }, 2000);
-}
-
-
-
+    let delay = 0;
+    for (let i = 0; i < 7; i++) {
+        setTimeout(() => {
+            document.querySelectorAll(".hidden").forEach((element, index) => {
+                if (index === 0) {
+                    i++;
+                } else { i = 3 }
+                element.style.opacity = i / 10;
+            });
+        }, delay);
+        delay += 300;
+    }
+};
 
 
 export default visible
+
+
+
