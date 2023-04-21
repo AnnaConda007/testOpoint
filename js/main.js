58fa58e418d1e8e2654486f049877cf3c73281b6
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-21 19:10:11 +0400

    скрол

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import slider from './slider.js'
-
+import scrol from './scroll.js'
 window.addEventListener("DOMContentLoaded", function () {
     slider()
+    scrol()
 })
--- js/slider.js
+++ js/slider.js
@@ -36,25 +36,6 @@ const slider = () => {
         }
     });
 
-    const wrap = document.querySelector(".content__text-wrap");
-    const scrolWrap = document.querySelector(".scrol")
-    const scrolThumb = document.querySelector('.scrol__thumb');
-
-    wrap.addEventListener("scroll", () => {
-        const scroll = wrap.scrollTop;
-        console.log(scroll)
-        scrolThumb.style.top = `${scroll}px`
-    });
-
-    const updateThumbPosition = (e) => {
-        const mouseY = e.clientY - scrolWrap.getBoundingClientRect().top;
-        scrolThumb.style.top = `${mouseY}px`;
-        wrap.scrollTop = `${mouseY}`;
-    };
-
-    scrolWrap.addEventListener("mousemove", (e) => {
-        updateThumbPosition(e)
-    })
 
 
 
