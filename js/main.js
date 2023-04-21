5bab5cf979a5402ce90d09329189e74c852e280f
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-21 23:29:03 +0400

    слайдер работает

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import slider from './slider.js'
 import scrol from './scroll.js'
 import visible from './hidden.js'
+import render from './renderModal.js'
 window.addEventListener("DOMContentLoaded", function () {
     slider()
     scrol()
     visible()
+    render()
 })
