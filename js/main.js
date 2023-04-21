dc09f20471e2f996a5102086977e775f362e2070
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-22 02:34:21 +0400

    home

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ import slider from './slider.js'
 import scrol from './scroll.js'
 import visible from './hidden.js'
 import render from './renderModal.js'
+import home from './home.js'
 window.addEventListener("DOMContentLoaded", function () {
     slider()
     scrol()
     visible()
     render()
+    home()
 })
--- js/renderModal.js
+++ js/renderModal.js
@@ -1,39 +1,22 @@
 const render = () => {
+
     const modal = document.querySelector(".content--modal")
     const trigger = document.querySelector(".content__btn")
     const hidden = document.querySelector(".content__wrap")
-
+    const slide = document.querySelector(".third-slide")
     trigger.addEventListener("click", () => {
         modal.style.display = "flex"
         hidden.style.display = "none"
+
     })
-
-
-
-
-
-
-
-
-
     modal.addEventListener("click", (e) => {
-        if (!e.target.closest(".content__wrap--modal") && modal.style.display == "flex") {
+        if (!e.target.closest('.content__modal')) {
+            console.log(e.target)
             modal.style.display = "none"
             hidden.style.display = "flex"
         }
+
     })
-
-
-
-
-
-
-
-
-
-
-
-
 
     const backBtn = document.querySelector(".nav-left")
     const nextBtn = document.querySelector(".nav-right")
