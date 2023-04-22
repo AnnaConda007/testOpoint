e53b393ab570dcf18892f42e2c478d11a6398486
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-22 14:39:32 +0400

    фон

--- js/hidden.js
+++ js/hidden.js
@@ -1,18 +1,18 @@
 const visible = () => {
     let delay = 0;
-    for (let i = 0; i < 7; i++) {
+    const opacity = 7
+    for (let i = 0; i < opacity; i++) {
         setTimeout(() => {
             document.querySelectorAll(".hidden").forEach((element, index) => {
-                if (index === 0) {
+                if (index == 0) {
                     i++;
                 } else { i = 3 }
                 element.style.opacity = i / 10;
             });
         }, delay);
-        delay += 300;
+        delay += 500;
     }
 };
-
 
 export default visible
 
